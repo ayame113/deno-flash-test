45d9a4caed23eadef42bb70143dd58966b9828d7
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-08-23 11:10:50 +0900

    try ci

---
+++ serve.ts
@@ -0,0 +1,27 @@
+const abortController = new AbortController();
+Deno.serve({
+  fetch() {
+    return new Response("hello world");
+  },
+  async onListen() {
+    const res1 = await fetch("http://localhost:9000/1");
+    console.log("res1: ", await res1.text());
+    const res2 = await fetch("http://localhost:9000/2");
+    console.log("res2: ", await res2.text());
+    abortController.abort();
+  },
+  signal: abortController.signal,
+});
+
+// import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
+//
+// serve(() => new Response("hello world"), {
+//   async onListen() {
+//     const res1 = await fetch("http://localhost:8000/1");
+//     console.log("res1: ", await res1.text());
+//     const res2 = await fetch("http://localhost:8000/2");
+//     console.log("res2: ", await res2.text());
+//     abortController.abort();
+//   },
+//   signal: abortController.signal,
+// });
